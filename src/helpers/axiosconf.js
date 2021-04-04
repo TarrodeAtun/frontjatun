3885d08bc34c73ad1d2dbca914c2c01533a08303
Claudio Vidal <you@example.com>
2021-04-04 15:40:10 -0400

    asd

--- src/helpers/axiosconf.js
+++ src/helpers/axiosconf.js
@@ -1,6 +1,6 @@
 var axios = require('axios');
 var Axios = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'http://ec2-18-219-217-99.us-east-2.compute.amazonaws.com:4000',
 });
 
 module.exports = Axios;
